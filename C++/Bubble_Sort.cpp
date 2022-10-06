d68cb3e874b302d8cf70f7b8c374c0f9bb26dc6f
Pawan <rksing4321@gmail.com>
2022-10-06 12:16:51 +0530

    Added InsertionSort and BubbleSort

---
+++ C++/Bubble_Sort.cpp
@@ -0,0 +1,58 @@
+// Optimized bubble sort in C++
+
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+// perform bubble sort
+void bubbleSort(int array[], int size) {
+    
+  // loop to access each array element
+  for (int step = 0; step < (size-1); ++step) {
+      
+    // check if swapping occurs
+    int swapped = 0;
+    
+    // loop to compare two elements
+    for (int i = 0; i < (size-step-1); ++i) {
+        
+      // compare two array elements
+      // change > to < to sort in descending order
+      if (array[i] > array[i + 1]) {
+
+        // swapping occurs if elements
+        // are not in intended order 
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        
+        swapped = 1;
+      }
+    }
+
+    // no swapping means the array is already sorted
+    // so no need of further comparison
+    if (swapped == 0)
+      break;
+  }
+}
+
+// print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    cout << "  " << array[i];
+  }
+  cout << "\n";
+}
+
+int main() {
+  int data[] = {-2, 45, 0, 11, -9};
+  
+  // find the array's length
+  int size = sizeof(data) / sizeof(data[0]);
+  
+  bubbleSort(data, size);
+  
+  cout<< "Sorted Array in Ascending Order:\n";
+  printArray(data, size);
+}
